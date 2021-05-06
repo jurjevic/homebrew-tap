3d3df14bc45527b913866a8adc3d9c6a52c5214e
Jurica Jurjevic <jurica.jurjevic@daimler.com>
2021-05-06 17:41:32 +0200

    splitvpn v0.9.6-beta added.

--- Formula/splitvpn.rb
+++ Formula/splitvpn.rb
@@ -1,8 +1,8 @@
 class Splitvpn < Formula
   desc "Split Internet and VPN routing"
   homepage "https://github.com/jurjevic/SplitVPN"
-  url "https://github.com/jurjevic/SplitVPN/archive/v0.9.5-beta.tar.gz" # ### ftoken[3] = "\"" + Download + "\""; Join(ftoken, " ")
-  sha256 "b33d8e9dae273521f2a80eb81a3e2e2b08d0e4fa81ddb27f501a00c19c2283d1" # ### ftoken[3] = "\"" + Hash + "\""; Join(ftoken, " ")
+  url "https://github.com/jurjevic/SplitVPN/archive/v0.9.6-beta.tar.gz" # ### ftoken[3] = "\"" + Download + "\""; Join(ftoken, " ")
+  sha256 "5fb9a03ed2714f323a149a881ff3f50e1110bfdb8eace97ad73b5ab2ec7d8604" # ### ftoken[3] = "\"" + Hash + "\""; Join(ftoken, " ")
   license "MIT"
 
   depends_on "go@1.16" => :build
