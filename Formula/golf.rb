46e83772537bf3e022c362e07bd49ab418885517
Jurica Jurjevic <jurica.jurjevic@daimler.com>
2021-05-06 21:51:07 +0200

    golf v0.10.0 added.

--- Formula/golf.rb
+++ Formula/golf.rb
@@ -1,7 +1,7 @@
 class Golf < Formula
   desc "Go Language Facilitator"
   homepage "https://github.com/jurjevic/golf"
-  url "https://github.com/jurjevic/golf/archive/v0.9.10-alpha.tar.gz" # ### ftoken[3] = "\"" + Download + "\""; Join(ftoken, " ")
+  url "https://github.com/jurjevic/golf/archive/v0.10.0.tar.gz" # ### ftoken[3] = "\"" + Download + "\""; Join(ftoken, " ")
   sha256 "36a99d718a46cb7f24e1193061c341f275180792c52db94955aca9d2191f3f43" # ### ftoken[3] = "\"" + Hash + "\""; Join(ftoken, " ")
   license "MIT"
 
